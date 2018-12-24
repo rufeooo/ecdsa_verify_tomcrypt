d1456b5191a5b0b362f90b92f50451b395826742
Alan Newton <rufeooo@gmail.com>
2018-12-24 08:01:52 -0800

    Convert ec .der format from openssl->tomcrypt

--- main.c
+++ main.c
@@ -60,7 +60,7 @@ size_t load_signature(const char* filename, unsigned char** out_buffer)
   return buffer_len;
 }
 
-int main()
+int verify_ossl_signature()
 {
   FILE* messageFile = fopen("message", "rb");
 
@@ -68,8 +68,8 @@ int main()
     exit(1);
 
   // Hash message
-  unsigned char hash[256 / 8];
-  sha256_contents(messageFile, hash);
+  unsigned char hash[256 / 8] = { 0xab, 0xfa, 0x5d, 0xfd, 0x7f, 0x90, 0xbe, 0xea, 0x2c, 0x7a, 0x06, 0x49, 0xbf, 0x63, 0xda, 0x17, 0x2f, 0x02, 0x4f, 0xe6, 0x9e, 0x56, 0xa1, 0x28, 0x84, 0xa7, 0xe8, 0xeb, 0xc9, 0x92, 0x5d, 0xc5};
+  /*sha256_contents(messageFile, hash);*/
 
   for (int i = 0; i < sizeof(hash); ++i)
   {
@@ -82,7 +82,7 @@ int main()
 
   // Load key
   ecc_key key;
-  const char* key_filename = "tom.der";
+  const char* key_filename = "out_tom.key";
   if (import_ecc_from(key_filename, &key) != CRYPT_OK)
     exit(3);
 
@@ -97,4 +97,101 @@ int main()
   signature = NULL;
 
   printf("Verify stat; %d\n", stat);
+  return 0;
+}
+
+unsigned char* offsetByte(unsigned char* buffer, long length, int bit)
+{
+  size_t offset = bit/8;
+  if (offset >= length)
+    return NULL;
+  return &buffer[offset];
+}
+
+int main()
+{
+  return verify_ossl_signature();
+}
+
+int der_convert_openssl_to_tomcrypt()
+{
+  FILE* eckey_file = fopen("ecdsa-p256-public.der", "rb");
+  if (eckey_file == NULL)
+    return 1;
+
+  fseek(eckey_file, 0, SEEK_END);
+  unsigned long eckey_len = ftell(eckey_file);
+  rewind(eckey_file);
+  unsigned char* eckey = malloc(eckey_len);
+  size_t item_len = eckey_len / sizeof(char);
+  if (fread(eckey, sizeof(char), item_len, eckey_file) != item_len)
+    return 2;
+
+  // Decode the openssl .der file
+  void *p, *q, *r, *s, *t;
+  int res;
+  ltc_asn1_list *obj_seq;
+
+  res = der_decode_sequence_flexi(eckey, &eckey_len, &obj_seq);
+  if (res != CRYPT_OK)
+    return 3;
+
+  printf("%lu bytes total, root seq len %lu used %d\n", eckey_len, obj_seq->size, obj_seq->used);
+  ltc_asn1_list *child1 = obj_seq->child;
+  printf("child1 seq type: %d len %lu used %d\n", child1->type, child1->size, child1->used);
+  ltc_asn1_list *sibling1 = child1->next;
+  printf("sibiling1 type: %d size: %lu used: %d %02X\n", sibling1->type, sibling1->size, sibling1->used, *(unsigned char*)sibling1->data);
+  unsigned char* data = sibling1->data;
+  size_t xy_buf_len = sibling1->size/8;
+  unsigned char* xy_buf = malloc(xy_buf_len);
+  printf("xy_buf_len %lu\n", xy_buf_len);
+  memset(xy_buf, 0, xy_buf_len);
+  for (int i = 0; i < sibling1->size; ++i)
+  {
+    if (data[i])
+      *offsetByte(xy_buf, xy_buf_len, i) |= 1 << (7-i%8);
+    printf("%c", data[i]?'1':'0');
+  }
+  printf("\n");
+  for (int i = 0; i < xy_buf_len; ++i)
+  {
+    printf("%02X", xy_buf[i]);
+  }
+  printf("\n");
+
+  // Init crypto math
+  ltc_mp = ltm_desc;
+
+  // Try writing a tomcrypt format
+  ecc_key outKey;
+  outKey.type = PK_PUBLIC;
+
+  unsigned char flags[1];
+  unsigned char out_buf[ECC_BUF_SIZE];
+  unsigned long buf_size = sizeof(out_buf);
+  unsigned long key_size = 32;
+  void *x, *y;
+  if (ltc_mp.init(&x) != CRYPT_OK)
+    return 4;
+  if (ltc_mp.init(&y) != CRYPT_OK)
+    return 4;
+  if (ltc_mp.unsigned_read(x, &xy_buf[1], key_size))
+    return 5;
+  if (ltc_mp.unsigned_read(y, &xy_buf[1+key_size], key_size))
+    return 5;
+  flags[0] = 0;
+  res = der_encode_sequence_multi(out_buf, &buf_size,
+      LTC_ASN1_BIT_STRING,      1UL, flags,
+      LTC_ASN1_SHORT_INTEGER,   1UL, &key_size,
+      LTC_ASN1_INTEGER,         1UL, x,
+      LTC_ASN1_INTEGER,         1UL, y,
+      LTC_ASN1_EOL,             0UL, NULL);
+
+  FILE* keyOut = fopen("out_tom.key", "wb+");
+  if (keyOut == NULL)
+    return 10;
+  fwrite(out_buf, sizeof(char), buf_size, keyOut);
+  fclose(keyOut);
+
+  return 0;
 }
